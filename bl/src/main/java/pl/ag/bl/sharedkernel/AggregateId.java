6efccd135173ea3de8c69e3b68bdbb29755d6189
Adam Gwóźdź <70854700+adamgwozdz00@users.noreply.github.com>
2021-07-27 22:46:18 +0200

    Add AggregateId and NutrionalValue

---
+++ bl/src/main/java/pl/ag/bl/sharedkernel/AggregateId.java
@@ -0,0 +1,53 @@
+package pl.ag.bl.sharedkernel;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+
+public class AggregateId implements Serializable {
+
+  private final String aggregateId;
+
+  public AggregateId(String aggregateId) {
+    if (aggregateId == null) {
+      throw new IllegalArgumentException("Id cannot be null");
+    }
+    this.aggregateId = aggregateId;
+  }
+
+  public static AggregateId generate() {
+    return new AggregateId(UUID.randomUUID().toString());
+  }
+
+  public String getAggregateId() {
+    return aggregateId;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof AggregateId)) {
+      return false;
+    }
+    AggregateId other = (AggregateId) o;
+    if (aggregateId == null) {
+      if (other.aggregateId != null) {
+        return false;
+      }
+    }
+    return aggregateId.equals(other.aggregateId);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(aggregateId);
+  }
+
+  @Override
+  public String toString() {
+    return aggregateId;
+  }
+}
---
+++ bl/src/main/java/pl/ag/bl/sharedkernel/NutritionalValue.java
@@ -0,0 +1,70 @@
+package pl.ag.bl.sharedkernel;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class NutritionalValue {
+
+  private BigDecimal nutritionalValue;
+
+  protected NutritionalValue() {
+  }
+
+  public NutritionalValue(double nutritionalValue) {
+    this.nutritionalValue = BigDecimal.valueOf(nutritionalValue);
+  }
+
+  public NutritionalValue(long nutritionalValue) {
+    this.nutritionalValue = BigDecimal.valueOf(nutritionalValue);
+  }
+
+  public void add(NutritionalValue nutritionalValue) {
+    this.nutritionalValue = this.nutritionalValue.add(nutritionalValue.nutritionalValue);
+  }
+
+  public void multiply(double multiple) {
+    this.nutritionalValue = this.nutritionalValue.multiply(BigDecimal.valueOf(multiple));
+  }
+
+  public void multiply(NutritionalValue multiple) {
+    this.nutritionalValue = this.nutritionalValue.multiply(multiple.nutritionalValue);
+  }
+
+  public void divide(NutritionalValue divisor) {
+    this.nutritionalValue = this.nutritionalValue.divide(divisor.nutritionalValue);
+  }
+
+  public boolean compare(NutritionalValue nutritionalValue) {
+    return this.nutritionalValue.compareTo(nutritionalValue.nutritionalValue) == 0;
+  }
+
+  public static NutritionalValue ZERO() {
+    return new NutritionalValue(0);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof NutritionalValue)) {
+      return false;
+    }
+    NutritionalValue that = (NutritionalValue) o;
+    return this.nutritionalValue.equals(that.nutritionalValue);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(nutritionalValue);
+  }
+
+  @Override
+  public String toString() {
+    return "NutritionalValue{" +
+        "nutritionalValue=" + nutritionalValue +
+        '}';
+  }
+}
